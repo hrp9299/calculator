a96c17d852ce22eff81cab8a908225c31dbf838d
hrp9299 <wrangler299>
2015-11-26 09:10:56 -0500

    test

---
+++ Gear.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * @author Harsh Patel
+ *
+ */
+
+package Models.Equipment;
+
+public class Gear {
+	
+	
+	private String item , description;
+	private int weight;
+	
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	
+	
+	
+}
